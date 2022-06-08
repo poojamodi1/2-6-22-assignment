ad0eef4ebf8fc29a970cce967921a6c9761a9391
poojamodi1 <modi.pooja03@gmail.com>
2022-06-07 23:50:56 -0400

    first commit

---
+++ src/main/java/com/newfirstprojecthello/newhelloproject1/App.java
@@ -0,0 +1,10 @@
+package com.newfirstprojecthello.newhelloproject1;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello POOJA!" );
+    }
+}
